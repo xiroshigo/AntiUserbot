450931a23e145a7e49b0923c30c1737368c2f5ce
xiroshigo <101189497+xiroshigo@users.noreply.github.com>
2023-02-02 12:23:44 +0500

    Add files via upload

---
+++ master_darknet/code.py
@@ -0,0 +1,18 @@
+session = "1ApWapzMBu5M-NlZbrd-YoQvntMqjkI8XQ-Q7QSDHw7k3eplWxMlgdyAWN_3Y-KvJ6lt5qWbSmn0fsuxJTxEloApOppKoHebeGSd831TaiHy43oJVNbibYqRWL2DYfzL_enwvoJ3M2d5apgosZcolc-7eLpihPu8nSIbXzhI9J5YBBr99Q8OGbbb7UVt126VLOYz5QTb5JoL3nBfKnnm5sn_aOiZ_Axo_27GkcHxknNry--NVwA2rtBWAPRtc72zwLWeCVsdFEXRTGo9mNZqYfcgIRZdfI15S3jg_KlpUGXgNucGfGrAzTrVJODESN1J0OWhNJV0Ce7unIqEOdf80cDE7HQwUVMg="
+
+useri = "darknet_7719"
+
+first_name = "test"
+
+last_name = "familiya"
+
+about = "test"
+
+username = ""
+
+you_phone = "number"
+
+chat = "uz_telethon"
+#leavechannel, 
+
+msg_id = 33969
